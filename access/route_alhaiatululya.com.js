42bf5d66788299a99734aa34400da9f1d2df7efd
asifulmamun <asifulmamun@gmail.com>
2021-07-18 15:26:57 +0600

    check

--- access/route_alhaiatululya.com.js
+++ access/route_alhaiatululya.com.js
@@ -1,1 +1,1 @@
-document.querySelector('.side_menu_li_3').innerHTML = '<a href="https://forms.gle/RdYSCZFhvSAu56t2A">সমস্যা হলে ক্লিক করুন</a>';
+// document.querySelector('.side_menu_li_3').innerHTML = '<a href="https://forms.gle/RdYSCZFhvSAu56t2A">সমস্যা হলে ক্লিক করুন</a>';

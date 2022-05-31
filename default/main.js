c1ab93d1360e21376dd2ead42af66d148a8a7dd4
asifulmamun <asifulmamun@gmail.com>
2022-05-31 10:11:34 +0600

    Update

---
+++ default/main.js
@@ -0,0 +1,13 @@
+alert('Hello');
+
+// // Developer
+// getJSON('https://raw.githubusercontent.com/asifulmamun/data/master/json/default/main.json', function (err, developer) {
+//     // If json data found
+//     if (err != null) {
+//         console.error(err);
+//     } else {
+
+//         console.log(developer.Company.CEO.UserName)
+
+//     }
+// });

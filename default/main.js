2272129320a044761080395a2cd04ab828d14594
asifulmamun <asifulmamun@gmail.com>
2022-06-01 12:03:37 +0600

    fix: getjosn function

--- default/main.js
+++ default/main.js
@@ -1,4 +1,22 @@
-alert('Hello');
+// Get Json
+var getJSON = function (url, callback) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', url, true);
+    xhr.responseType = 'json';
+
+    xhr.onload = function () {
+
+        var status = xhr.status;
+
+        if (status == 200) {
+            callback(null, xhr.response);
+        } else {
+            callback(status);
+        }
+    };
+
+    xhr.send();
+};
 
 // Developer
 getJSON('./main.json', function (err, developer) {

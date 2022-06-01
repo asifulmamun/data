08949e1d3cb909fbc09bf43fbf20fbd4046114ad
asifulmamun <asifulmamun@gmail.com>
2022-06-01 12:00:21 +0600

    feture: index.html added for git page

--- default/main.js
+++ default/main.js
@@ -1,13 +1,13 @@
 alert('Hello');
 
-// // Developer
-// getJSON('https://raw.githubusercontent.com/asifulmamun/data/master/json/default/main.json', function (err, developer) {
-//     // If json data found
-//     if (err != null) {
-//         console.error(err);
-//     } else {
+// Developer
+getJSON('./main.json', function (err, developer) {
+    // If json data found
+    if (err != null) {
+        console.error(err);
+    } else {
 
-//         console.log(developer.Company.CEO.UserName)
+        console.log(developer.Company.CEO.UserName)
 
-//     }
-// });
+    }
+});
